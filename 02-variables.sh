4abdd41e8ea7822a2daed24469119049cafd2db5
Santosh kumar <santosh30901@gmail.com>
2026-08-27 21:38:53 +0530

    first file

---
+++ 02-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1: How are you Suresh"
+echo "$PERSON2: Hi Ramesh, Im good"
+echo "$PERSON1: what are you learning"
+echo "$PERSON2 : Im learning Devops cource"
