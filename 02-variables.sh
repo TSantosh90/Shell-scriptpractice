7684cd44b66c02547aa1dd61547e58f0bd75f455
Santosh kumar <santosh30901@gmail.com>
2026-08-28 08:15:37 +0530

    change

--- 02-variables.sh
+++ 02-variables.sh
@@ -3,7 +3,7 @@
 PERSON1=$1
 PERSON2=$2
 
-echo "$PERSON1: How are you Suresh"
-echo "$PERSON2: Hi Ramesh, Im good"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: Hi $PERSON1, Im good"
 echo "$PERSON1: what are you learning"
 echo "$PERSON2 : Im learning Devops cource"
