6daf68de04383a3b1939f25b1eaa00ac6604d1bc
Santosh kumar <santosh30901@gmail.com>
2026-08-29 15:28:00 +0530

    change

---
+++ 08-functions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run with sudo access"
+    exit 1 
+fi
+
+VALIDATE(){
+if [ $1 -ne 0 ]; then
+    echo "$2....failure"
+    exit 1 
+else
+echo "$2...Success"
+fi
+}
+
+dnf install nginx -y
+VALIDATE $? "Installing Nginx"
+
+dnf install mysql -y
+VALIDATE $? "Installing Mysql"
+
+dnf install nodejs -y
+VALIDATE $? "Installing nodejs"
