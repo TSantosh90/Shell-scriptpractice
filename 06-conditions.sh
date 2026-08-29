9e818e9f4796cf41ce4a530c507ee5b7f47b8334
Santosh kumar <santosh30901@gmail.com>
2026-08-29 10:37:01 +0530

    change

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -4,6 +4,6 @@ NUMBER=$1
 
 if [ $NUMBER -gt 20 ]; then
     echo "Given number= $NUMBER is greater than 20"
-else [$NUMBER -eq 20 ]; then
+elif [$NUMBER -eq 20 ]; then
     echo "Given number=$NUMBER is equal to 20"
 fi
